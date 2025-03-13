1f65e882d360756d932f43e260577e6b508f1b5a
Austin <austinprez05@gmail.com>
2025-03-12 22:12:01 -0700

    first commit

---
+++ CCNameGen.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// I've been watching a lot of CreepCast
+// and thought it'd be fun to make a creepy pasta
+// name generator. So this is it.
+
+int main()
+{
+    cout << "-----------------------------" << endl
+    << "Generate Name Type 1 (G)" << endl
+    << "EX: Eyeless Jack, Laughing Jack" << endl
+    << "Generate Name Type 2 (F)" << endl
+    << "EX: Jeff the Killer, Ben Drowned" << endl
+    << "Quit (Q)" << endl
+    << "-----------------------------" << endl;
+    cout << "Please pick an option: ";
+    //Put switch case here
+    //Going to have three random generators
+    //two for the adjectives and one for the name.
+
+    return 0;
+}
