5c43eb09aca19d7d98dd80a1b5deb4b7c620353b
Austin <austinprez05@gmail.com>
2025-03-17 18:55:57 -0700

    Commit 2

--- CCNameGen.cpp
+++ CCNameGen.cpp
@@ -5,19 +5,30 @@ using namespace std;
 // and thought it'd be fun to make a creepy pasta
 // name generator. So this is it.
 
+//Global access functions
+
 int main()
 {
+    char choice;
     cout << "-----------------------------" << endl
-    << "Generate Name Type 1 (G)" << endl
-    << "EX: Eyeless Jack, Laughing Jack" << endl
-    << "Generate Name Type 2 (F)" << endl
-    << "EX: Jeff the Killer, Ben Drowned" << endl
-    << "Quit (Q)" << endl
-    << "-----------------------------" << endl;
+         << "Generate Name Type 1 (G)" << endl
+         << "EX: Eyeless Jack, Laughing Jack" << endl
+         << "Generate Name Type 2 (F)" << endl
+         << "EX: Jeff the Killer, Ben Drowned" << endl
+         << "Quit (Q)" << endl
+         << "-----------------------------" << endl;
     cout << "Please pick an option: ";
-    //Put switch case here
-    //Going to have three random generators
-    //two for the adjectives and one for the name.
+    cin >> choice;
+    
+    switch(choice)
+    {
+        case 'Q': cout << "Exiting program." << endl;
+        exit(0);
+        break;
+        default: cout << "You did not enter a valid input." << endl;
+    }
+    // Going to have three random generators
+    // two for the adjectives and one for the name.
 
     return 0;
 }
